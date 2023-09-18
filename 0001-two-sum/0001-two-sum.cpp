d796bc045ba94e3a3fe5e122b75e7d02050a9926
Arya Vijayan <aryavijayanrohini@gmail.com>
2023-09-18 07:32:44 +0530

    Time: 7 ms (90.77%), Space: 11.2 MB (14.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,19 @@
+
+// One pass hash table - check the table if the complement already exists for the currently inserting element
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=size(nums),i,j,flag=0;
-        vector<int> arr;
-        for(i=0;i<n;i++)
-          for(j=0;j<n;j++)
-            if((nums[i]+nums[j])==target && i!=j && flag==0) 
-             {
-                arr.push_back(i);
-                arr.push_back(j);
-                flag=1;
-             } 
-        
-        return arr;
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
     }
 };
