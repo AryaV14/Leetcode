0e9a9cf81628c831681cd5f1633307316203c328
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-02-01 23:13:41 +0530

    Time: 20 ms (94.60%) | Memory: 22.6 MB (19.35%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> temp;
-        int t=0;
+        vector<vector<int>> ans;
+        int n= intervals.size();
         sort(intervals.begin(), intervals.end());
-        temp = intervals;
-        for(int i=0;i<intervals.size();i++)
-        {
-            if(temp[t][1]>=intervals[i][0] && temp[t][1]<=intervals[i][1])
-            {
-                temp[t][1]=intervals[i][1];
-            }else if(temp[t][1]>=intervals[i][0] && temp[t][1]>=intervals[i][1])
-            {
-                continue;
+        for(int i=0;i<n; i++){
+            if(ans.empty() || ans.back()[1] < intervals[i][0]){
+                ans.push_back(intervals[i]);
             }
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+        }
+        return ans;
 
-            else
-            {
-                t++;
-                temp[t]= intervals[i];
-            }
-        }
-        t++;
-        while(t<intervals.size())
-        {
+
+
+
+        // vector<vector<int>> temp;
+        // int t=0;
+        // sort(intervals.begin(), intervals.end());
+        // temp = intervals;
+        // for(int i=0;i<intervals.size();i++)
+        // {
+        //     if(temp[t][1]>=intervals[i][0] && temp[t][1]<=intervals[i][1])
+        //     {
+        //         temp[t][1]=intervals[i][1];
+        //     }else if(temp[t][1]>=intervals[i][0] && temp[t][1]>=intervals[i][1])
+        //     {
+        //         continue;
+        //     }
+
+        //     else
+        //     {
+        //         t++;
+        //         temp[t]= intervals[i];
+        //     }
+        // }
+        // t++;
+        // while(t<intervals.size())
+        // {
            
-            temp.erase(temp.begin() + t);
-            t++;
-        }
-        return temp;
+        //     temp.erase(temp.begin() + t);
+        //     t++;
+        // }
+        // return temp;
     }
 };
