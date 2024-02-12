fb7b7733d224072414b3c31c2298e2f87c7de777
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-02-12 08:35:16 +0530

    Time: 15 ms (76.01%) | Memory: 21.7 MB (14.58%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.size();i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
-            }
-            else if (snowBallSize > 0) {
-	            int t = nums[i];
-	            nums[i]=0;
-	            nums[i-snowBallSize]=t;
-            }
+       int j = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] != 0)
+                nums[j++] = nums[i];
         }
+        fill(nums.begin() + j, nums.end(), 0);
     
     }
 };
