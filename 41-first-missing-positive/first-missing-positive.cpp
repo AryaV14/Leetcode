61a6369a88da26bb1022a30f2ac42057ea233240
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-05-23 19:32:26 +0530

    Time: 46 ms (52.35%) | Memory: 53.4 MB (25.54%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,32 +1,58 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int small,large;
-        small=INT_MAX;
-        large=nums[0];
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-        {
-            m[nums[i]]=i;
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]<small && nums[i]>0)
-                small=nums[i];
-            if(nums[i]>large && nums[i]>0)
-                large=nums[i];
+        
+        int n = nums.size();
+        nums.push_back(0);
 
-        }
-        if(small>1)
-            return 1;
-        for(int i=small;i<=large;i++)
+        for (int i = 0 ; i < n; ++i)
         {
-            if(m.find(i)!=m.end())
-                continue;
-            else
+            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]]){
+                swap(nums[i], nums[nums[i]]);
+            }
+        }
+
+        for (int i = 1; i <= n; ++i)
+            if (nums[i] != i){
                 return i;
-
-        }
-        return large+1;
+            }
+            
+        return n + 1;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int small,large;
+//         small=INT_MAX;
+//         large=nums[0];
+//         unordered_map<int,int> m;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             m[nums[i]]=i;
+//         }
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]<small && nums[i]>0)
+//                 small=nums[i];
+//             if(nums[i]>large && nums[i]>0)
+//                 large=nums[i];
+
+//         }
+//         if(small>1)
+//             return 1;
+//         for(int i=small;i<=large;i++)
+//         {
+//             if(m.find(i)!=m.end())
+//                 continue;
+//             else
+//                 return i;
+
+//         }
+//         return large+1;
+//     }
+// };
