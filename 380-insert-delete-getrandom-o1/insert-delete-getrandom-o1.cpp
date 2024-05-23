260fe410a8fbaba9eb33fd332d31b7ea14a54d60
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-05-23 18:55:03 +0530

    Time: 160 ms (43.28%) | Memory: 100.6 MB (44.25%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,49 +1,34 @@
 class RandomizedSet {
-    vector<int> v;
-    unordered_map<int,int> mp;
 public:
+    /** Initialize your data structure here. */
     RandomizedSet() {
+        
     }
     
-    bool search(int val){
-
-         if(mp.find(val)!=mp.end())
-            return true;
-         return false;
-
-    }
-
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        if(search(val))
-            return false;
-
-        v.push_back(val);
-        mp[val] = v.size()-1;
+        if (m.find(val) != m.end()) return false;
+        nums.emplace_back(val);
+        m[val] = nums.size() - 1;
         return true;
     }
     
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        if(!search(val))
-            return false;
-
-       
-        auto it = mp.find(val);
-        v[it->second] = v.back();
-        v.pop_back();
-        mp[v[it->second]] = it->second;
-        mp.erase(val);
+        if (m.find(val) == m.end()) return false;
+        int last = nums.back();
+        m[last] = m[val];
+        nums[m[val]] = last;
+        nums.pop_back();
+        m.erase(val);
         return true;
     }
     
+    /** Get a random element from the set. */
     int getRandom() {
-        return v[rand()%v.size()];
+        return nums[rand() % nums.size()];
     }
+private:
+    vector<int> nums;
+    unordered_map<int, int> m;
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
