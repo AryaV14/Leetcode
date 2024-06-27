877302b99f0f1b13bf0b9c089a83cd9983d5a843
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-06-27 12:49:39 +0530

    Time: 84 ms (25.61%) | Memory: 70.1 MB (92.67%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -7,14 +7,9 @@ public:
         maxSum=INT_MIN;
         for(int i=0;i<n;i++)
         {
-            
             pre=max(nums[i],nums[i]+pre);
             maxSum=max(pre,maxSum);
-            // if(nums[i]>large)
-            //     large=nums[i];
         }
-        // if(maxSum==0)
-        //    maxSum =large;
         return maxSum;      
     }
 };
