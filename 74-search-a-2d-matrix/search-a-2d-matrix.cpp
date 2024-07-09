4f9ab176896d228452419af01787309081c42944
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-07-09 23:22:26 +0530

    Time: 7 ms (22.04%) | Memory: 12 MB (49.44%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -42,44 +42,5 @@ public:
         }
 
         return false;
-
-
-
-
-
-
-        
-        // int m,n,mid,low,high,row,x;
-        // x= target;
-        // m=matrix.size();
-        // n=matrix[0].size();
-        // low = 0;
-        // high = m-1;
-        // while (low <= high) 
-        // {
-        //     int mid = low + (high - low) / 2;
-        //     if(x>=matrix[mid][0] && x<=matrix[mid][n-1])
-        //     {
-        //         row = mid;
-        //         break;
-        //     }
-        //     if (matrix[mid][0]< x)
-        //         low = mid + 1;
-        //     else
-        //         high = mid - 1;
-        // }
-        // low = 0;
-        // high = matrix[row].size()-1;
-        // while (low <= high) 
-        // {
-        //     int mid = low + (high - low) / 2;
-        //     if (matrix[row][mid] == x)
-        //         return true;
-        //     if (matrix[row][mid]< x)
-        //         low = mid + 1;
-        //     else
-        //         high = mid - 1;
-        // }
-        // return false;
     }
 };
