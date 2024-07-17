6b1bef2ec8433d72330cf9d7399466b21c028f51
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-07-17 07:51:45 +0530

    Time: 55 ms (66.92%) | Memory: 53.9 MB (33.39%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,23 +1,60 @@
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n=nums.size(),small=INT_MAX;
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++)
+        
+        int n = nums.size();
+        nums.push_back(0);
+
+        for (int i = 0 ; i < n; ++i)
         {
-            mp[nums[i]]=i;
-            if(nums[i]>0)
-                small= min(small,nums[i]);
-        } 
-        if(small!=1)
-            return 1;
-        for(int i=0;i<n;i++)
-        {
-            if(mp.find(small+1)!=mp.end())
-                small = small+1;
-            else 
-                break;
+            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]]){
+                swap(nums[i], nums[nums[i]]);
+            }
         }
-        return small+1;
+
+        for (int i = 1; i <= n; ++i)
+            if (nums[i] != i){
+                return i;
+            }
+            
+        return n + 1;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int small,large;
+//         small=INT_MAX;
+//         large=nums[0];
+//         unordered_map<int,int> m;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             m[nums[i]]=i;
+//         }
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]<small && nums[i]>0)
+//                 small=nums[i];
+//             if(nums[i]>large && nums[i]>0)
+//                 large=nums[i];
+
+//         }
+//         if(small>1)
+//             return 1;
+//         for(int i=small;i<=large;i++)
+//         {
+//             if(m.find(i)!=m.end())
+//                 continue;
+//             else
+//                 return i;
+
+//         }
+//         return large+1;
+//     }
+// };
+
