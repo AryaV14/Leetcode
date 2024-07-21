5945ead756b04c504f74bf4255cd56ea57deb0d7
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-07-21 22:32:29 +0530

    Time: 66 ms (32.48%) | Memory: 61.7 MB (7.46%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -5,10 +5,8 @@ public:
         j=n-1;
         for(int i=0;i<j;)
         {
-           
             h=min(height[i],height[j]);
-            int x=h*(j-i);
-            maxarea=max(maxarea,x);
+            maxarea=max(maxarea,h*(j-i));
              if(height[i]>height[j] && j>i) 
                 j--;
             else
