0aa61af6268a49e6a31c09084e405a73d5294c76
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-07-21 20:53:41 +0530

    Time: 33 ms (86.82%) | Memory: 36.2 MB (62.44%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int,int> map;
+        int j;
         vector<int> ans;
         for(int i=0;i<nums.size();i++)
         {
-            if(map.find(nums[i])!=map.end())
+            j=abs(nums[i]);
+            if(nums[j-1]<0)
             {
-                ans.push_back(nums[i]);
+                ans.push_back(j);
             }
-            map[nums[i]]=i;
+            nums[j-1]*=-1;
+
         }
         return ans;
     }
 };
+
+// hashmap
+
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         unordered_map<int,int> map;
+//         vector<int> ans;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(map.find(nums[i])!=map.end())
+//             {
+//                 ans.push_back(nums[i]);
+//             }
+//             map[nums[i]]=i;
+//         }
+//         return ans;
+//     }
+// };
