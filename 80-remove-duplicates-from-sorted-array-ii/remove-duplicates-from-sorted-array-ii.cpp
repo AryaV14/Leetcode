ed8e58e5e20c28bc300659fc54c4e42e34ed051c
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-08-29 14:29:45 +0530

    Time: 8 ms (20.83%) | Memory: 13.7 MB (61.07%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -4,7 +4,7 @@ public:
         int index=2;
         if(nums.size()<=2)
         {
-            return nums.size();
+            return index=nums.size();
         }
         for(int i=2;i<nums.size();i++)
         {
