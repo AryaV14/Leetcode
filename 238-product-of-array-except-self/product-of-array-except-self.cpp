2e6a8b6d0b3ef9194b89d9738f4f2838d08be250
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-09-05 07:54:02 +0530

    Time: 26 ms (65.51%) | Memory: 38.3 MB (91.72%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n= size(nums);
+        int n=nums.size(),pre=1;
         vector<int> suf(n,1);
-        int k=1;
         for(int i=n-2;i>=0;i--)
-        {   
+        {
             suf[i]=suf[i+1]*nums[i+1];
         }
         for(int i=1;i<n;i++)
-        {   
-            k*=nums[i-1];
-            suf[i]*=k;
-        
-        }
-       
-        return suf;
-    
+        {
+            pre*=nums[i-1];
+            suf[i]*=pre;
 
-        // vector<int> pre;
-        // vector<int> suf;
-        // vector<int> res;
-        // int n=nums.size(),pdt1=1,pdt2=1;
-        // for(int i=0;i<n;i++)
-        // {
-        //     pdt1*=nums[i];
-        //     pdt2*=nums[n-i-1];
-        //     pre.push_back(pdt1);
-        //     suf.insert(suf.begin(),pdt2);
-        // }
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(i==0)
-        //         res.push_back(suf[i+1]);
-        //     else if(i==n-1)
-        //         res.push_back(pre[i-1]);
-        //     else
-        //    {    
-        //         pdt1=pre[i-1]*suf[i+1];
-        //         res.push_back(pdt1);
-        //     }
-            
-        // }
-        // return res;
+        }
+        return suf;
+
     }
 };
